892d5c64c49a01b19d8ff96f58aed70a9aa807a8
Nicolas Webb <nicw103@gmail.com>
2025-05-18 19:13:32 +0200

    Added connection to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
+import { getEnvironmentVariables } from "./environments/environment";
 
 let app: express.Application = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+mongoose.connect(getEnvironmentVariables().db_url).then(() => {
+  console.log("Connected to MongoDB");
 });
